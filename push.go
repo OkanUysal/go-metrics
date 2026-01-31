27440b8d349f9f1e6bf35990dbcb7951c262e80d
Okan UYSAL <uysal.okan.2010@gmail.com>
2026-01-31 22:45:32 +0300

    feat: Prometheus Remote Write protocol with Snappy compression

--- push.go
+++ push.go
@@ -8,7 +8,9 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/prometheus/common/expfmt"
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/snappy"
+	"github.com/prometheus/prometheus/prompb"
 )
 
 // StartGrafanaPush starts pushing metrics to Grafana Cloud
@@ -52,24 +54,80 @@ func (m *Metrics) pushToGrafana() error {
 		return fmt.Errorf("failed to gather metrics: %w", err)
 	}
 
-	// Encode metrics in Prometheus text format
-	var buf bytes.Buffer
-	encoder := expfmt.NewEncoder(&buf, expfmt.NewFormat(expfmt.TypeTextPlain))
-	
+	// Convert to Prometheus remote write format
+	var timeseries []*prompb.TimeSeries
+	now := time.Now().UnixMilli()
+
 	for _, mf := range metricFamilies {
-		if err := encoder.Encode(mf); err != nil {
-			return fmt.Errorf("failed to encode metric: %w", err)
+		for _, metric := range mf.GetMetric() {
+			// Create labels
+			labels := []*prompb.Label{
+				{Name: "__name__", Value: mf.GetName()},
+			}
+			for _, label := range metric.GetLabel() {
+				labels = append(labels, &prompb.Label{
+					Name:  label.GetName(),
+					Value: label.GetValue(),
+				})
+			}
+
+			// Get metric value
+			var value float64
+			switch mf.GetType() {
+			case 0: // COUNTER
+				if metric.Counter != nil {
+					value = metric.Counter.GetValue()
+				}
+			case 1: // GAUGE
+				if metric.Gauge != nil {
+					value = metric.Gauge.GetValue()
+				}
+			case 2: // SUMMARY
+				if metric.Summary != nil {
+					value = metric.Summary.GetSampleSum()
+				}
+			case 4: // HISTOGRAM
+				if metric.Histogram != nil {
+					value = metric.Histogram.GetSampleSum()
+				}
+			}
+
+			timeseries = append(timeseries, &prompb.TimeSeries{
+				Labels: labels,
+				Samples: []*prompb.Sample{
+					{
+						Value:     value,
+						Timestamp: now,
+					},
+				},
+			})
 		}
 	}
 
+	// Create write request
+	writeRequest := &prompb.WriteRequest{
+		Timeseries: timeseries,
+	}
+
+	// Marshal to protobuf
+	data, err := proto.Marshal(writeRequest)
+	if err != nil {
+		return fmt.Errorf("failed to marshal protobuf: %w", err)
+	}
+
+	// Compress with Snappy
+	compressed := snappy.Encode(nil, data)
+
 	// Create HTTP request
-	req, err := http.NewRequest("POST", m.config.GrafanaCloudURL, &buf)
+	req, err := http.NewRequest("POST", m.config.GrafanaCloudURL, bytes.NewReader(compressed))
 	if err != nil {
 		return fmt.Errorf("failed to create request: %w", err)
 	}
 
 	// Set headers
-	req.Header.Set("Content-Type", "application/openmetrics-text; version=1.0.0; charset=utf-8")
+	req.Header.Set("Content-Encoding", "snappy")
+	req.Header.Set("Content-Type", "application/x-protobuf")
+	req.Header.Set("X-Prometheus-Remote-Write-Version", "0.1.0")
 	req.Header.Set("User-Agent", "go-metrics/1.0")
 	
 	// Set basic auth
